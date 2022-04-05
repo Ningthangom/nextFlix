180ad5199c97c3d900ae15eff82c0166bf139ee6
Ningthangom <ningthangom@gmail.com>
2022-04-05 16:41:29 +0930

    created nav component

--- components/Nav/navbar.tsx
+++ components/Nav/navbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import styles from './navbar.module.css';
+import Link from "next/link";
 
 interface Props{ 
     username: string;
@@ -7,13 +9,29 @@ interface Props{
 const Navbar: React.FC<Props> = ({username}: Props) => {
 
   return (
-    <div>
-      Navbar
-      <p>{username}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Link href="/">
+          <a className={styles.logoLink}> Logo </a>
+        </Link>
+
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+            </button>
+            {/* expand more icons */}
+            <div className={styles.navDropdown}>
+              <a>Sign Out</a>
+              <div className={styles.lineWrapper}> </div>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
