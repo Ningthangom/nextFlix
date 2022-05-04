8def513207118b27966824979fb8870e66421a6d
Ningthangom <ningthangom@gmail.com>
2022-05-04 10:10:40 +0930

    added authcheck in _app for whole app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import {magic} from '../lib/magic-client'
+import {useRouter} from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
+  const router = useRouter();
+  useEffect(() => {
+    async function AuthCheck ()  {
+      
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if (isLoggedIn) {
+        // route to "/"
+        router.push("/")
+      }else{
+        // route to "/login"
+        router.push("/login")
+      }
+    }
+    AuthCheck();
+    
+  },[])
+
   return <Component {...pageProps} />
 }
 
