0441b707ce1807e30a6722c26a53b879189cf85d
Ningthangom <ningthangom@gmail.com>
2022-05-04 12:44:02 +0930

    added loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
 import {magic} from '../lib/magic-client'
-import {useRouter} from 'next/router'
+import {useRouter} from 'next/router';
+import Loading from '../components/loading/loading'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
+
   const router = useRouter();
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     async function AuthCheck ()  {
       
@@ -16,6 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         router.push("/")
       }else{
         // route to "/login"
+         
         router.push("/login")
       }
     }
@@ -23,7 +26,24 @@ function MyApp({ Component, pageProps }: AppProps) {
     
   },[])
 
-  return <Component {...pageProps} />
+  // this useEffect will solve homepage flickering issue
+  useEffect(() => {
+    // this will make sure that "sign in" is not re-rendered after
+    // geting didtoken
+    const handleComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
+  return loading ? <Loading />: <Component {...pageProps} />
 }
 
 export default MyApp
